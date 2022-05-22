dd9cf7ec969e2fad4b912ced8ccaa1c03309eea0
chungentsai <chungentsai1007@gmail.com>
2022-05-22 18:57:02 +0800

    modify server.py, add const.py and .gitignore

---
+++ qrcode/const.py
@@ -0,0 +1,9 @@
+# MySQL login information
+ROOT_ACCOUNT = "root"
+ROOT_PASSWORD = "chungentsai891007mac"
+DATABASE = None
+
+# guest account basic
+ACCOUNT_LEN = 16
+PASSWORD_LEN = 16
+DAILY_SESSION_TIME = 300 # in seconds
--- qrcode/server.py
+++ qrcode/server.py
@@ -1,11 +1,17 @@
 # Python 3 server example
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import time
-
-from utils import get_random_string, generate_qrcode
+import utils
+import const
 
 
 class HandleServer(BaseHTTPRequestHandler):
+    cnx = utils.create_mysql_connection("127.0.0.1",
+            const.ROOT_ACCOUNT, const.ROOT_PASSWORD, const.DATABASE)
+    # new guest account and password
+    account = None
+    password = None
+
     def __init__(self, *args, **kwargs):
         super(HandleServer, self).__init__(*args, **kwargs)
 
@@ -15,7 +21,6 @@ class HandleServer(BaseHTTPRequestHandler):
             self._handle_root()
         elif self.path.startswith('/qrcode'):
             self._handle_qrcode()
-        return
 
     def _handle_root(self):
         self.send_response(200)
@@ -25,24 +30,36 @@ class HandleServer(BaseHTTPRequestHandler):
         self.wfile.write(bytes("<center><img src='qrcode' alt='Something went wrong'></center>", "utf-8"))
         self.wfile.write(bytes("<meta http-equiv='refresh' content='1'>", "utf-8"))
         self.wfile.write(bytes("</body></html>", "utf-8"))
-        return
-    
+
     def _handle_qrcode(self):
-        generate_qrcode(get_random_string(length = 20))
+        if HandleServer._guest_account_is_used():
+            utils.generate_qrcode(HandleServer._create_guest_account())
         self.send_response(200)
         self.send_header("Content-type", "image/png")
         self.end_headers()
         with open('./qrcode.png', 'rb') as f:
             self.wfile.write(f.read())
-        return
+
+    @classmethod
+    def _create_guest_account(cls):
+        cls.account = utils.get_random_string(length=const.ACCOUNT_LEN)
+        cls.password = utils.get_random_string(length=const.PASSWORD_LEN)
+        # TODO: save into database
+        return cls.account + cls.password
+
+    @classmethod
+    def _guest_account_is_used(cls):
+        if cls.account is None:
+            return True
+        # TODO
+        return False
 
 
 class QRCodeServer:
-    def __init__(self, hostname = '140.112.30.57', port = 8081):
+    def __init__(self, hostname='127.0.0.1', port=8081):
         self.hostname = hostname
         self.port = port
         self.webserver = HTTPServer((self.hostname, self.port), HandleServer)
-
         print(f'[SERVER] - http://{hostname}:{port}')
 
     def run(self):
@@ -51,12 +68,12 @@ class QRCodeServer:
         except KeyboardInterrupt:
             print('[EXIT WITH KEYBOARDINTERRUPT]')
         self.webserver.server_close()
-        return
 
 
 def main():
-    server = QRCodeServer()
+    server = QRCodeServer(hostname='127.0.0.1', port=8081)
     server.run()
+
 
 if __name__ == "__main__":
     main()
